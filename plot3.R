2b6504108f059dc6b98e275b8eac48a4b43f11e5
Mohamed Slim Ben Mosbeh <mosbehsim@yahoo.fr>
2020-10-04 17:17:21 +0100

    the 3rd R file

--- plot3.R
+++ plot3.R
@@ -17,8 +17,12 @@ coln = read.table(textConnection(Mynames),sep = ";")
 names(mydf) = coln[1,]
 
 datetime <- strptime(paste(mydf$Date, mydf$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-png("plot2.png", width=480, height=480)
-plot(datetime, mydf$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+png("plot3.png", width=480, height=480)
+plot(datetime, mydf$Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, mydf$Sub_metering_2, type="l", col="red")
+lines(datetime, mydf$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off() 
 
 
