e989d405c13b1c5a3ae947b3acb69e8675636d1f
Mohamed Slim Ben Mosbeh <mosbehsim@yahoo.fr>
2020-10-04 17:13:52 +0100

    correction of plot 2

--- plot2.R
+++ plot2.R
@@ -16,8 +16,8 @@ coln = read.table(textConnection(Mynames),sep = ";")
 #Renaming the col names of the data frame
 names(mydf) = coln[1,]
 
-png("plot1.png", width=480, height=480)
-hist(mydf$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+png("plot2.png", width=480, height=480)
+plot(datetime, mydf$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
 
 
